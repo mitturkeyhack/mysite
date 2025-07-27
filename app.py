cf87f0766fa001c0776b9d7643060beb6d59fdf8
mitturkeyhack <akbmitistihbarat@gmail.com>
2025-07-27 23:18:46 +0300

    İlk canlı site

--- app.py
+++ app.py
@@ -1,34 +1,65 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.secret_key = "gizli-anahtar"
+app.secret_key = "gizli_anahtar"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+# Login ayarları
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Mail ayarları
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'seninmailin@gmail.com'
+app.config['MAIL_PASSWORD'] = 'uygulama_şifresi'  # Gmail'de 2FA varsa uygulama şifresi alman gerek
+mail = Mail(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/profile')
-def profile():
-    if not session.get("user"):
-        return redirect(url_for("login"))
-    return render_template('profile.html', user=session["user"])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
-        session['user'] = email
-        return redirect(url_for('profile'))
+        user = User.query.filter_by(email=email).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('profile'))
+        else:
+            flash('Kullanıcı bulunamadı!')
     return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
