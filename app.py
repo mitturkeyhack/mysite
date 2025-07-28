af1ba4176d1a5f26a49e3ea3ce2a205a59744936
senin_adın <epostan@gmail.com>
2025-07-28 11:20:41 +0300

    Render ve GitHub için son güncellemeler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return "<h1>Login sayfası</h1>"
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
